9b303370c9ae0e3ae8cd2cdd636aa875c1b74c54
arturtembe <malunganeartur@gmail.com>
2024-06-24 03:36:46 +0200

    three update

--- src/modules/helpers/endpoint.helper.js
+++ src/modules/helpers/endpoint.helper.js
@@ -1,5 +1,6 @@
 
-const host = `http://localhost:3333/mongodb/dash`;
+//onst host = `http://localhost:3333/mongodb/dash`;
+const host = `https://my-mongodb-617s.onrender.com/mongodb/dash`;
 
 const endpoint = {
     register: `${host}/register`,
